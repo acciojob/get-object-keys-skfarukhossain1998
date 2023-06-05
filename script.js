0012ad38654ab8ef5361d2e603b73a18998a559b
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-06-05 12:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const student={
+	name:"faruk";
+};
+Object.prototype.getKeys=function() {
+	return Object.keys(this);
+};
+
+const keys = student.getKeys();
+console.log(keys);
